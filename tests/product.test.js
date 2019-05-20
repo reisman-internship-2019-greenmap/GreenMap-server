6332bd813a384d1b7ef46ec383048cc367868621
eliotscott <eliotvscott@gmail.com>
2019-05-20 15:29:59 -0700

    [#19] Removed broken tests.

--- tests/product.test.js
+++ tests/product.test.js
@@ -1,9 +1,7 @@
 const expect = require('expect');
 const request = require('supertest');
 const {ObjectID} = require('mongodb');
-
-const {app} = require('./../server');
-const {Product} = require('../schemas/product');
+const { models, connectDb } = require ('../dal/database');
 
 // dummy documents
 const products = [{
@@ -18,43 +16,10 @@ const products = [{
 
 // runs before each test cases - populates the collection with the dummy documents after deleting all the documents from the collection
 beforeEach((done) => {
-  Product.deleteMany({})
-    .then(() => {
-      return Product.insertMany(products);
-    }).then(() => {
-      done();
-    });
+  done();
 });
 
 // Test suite
 describe('GET /products/:id', () => {
-  // Test case
-  it('should return product document', (done) => {
-    request(app)
-      .get(`/products/${products[0]._id.toHexString()}`)
-      .expect(200)
-      .expect((res) => {
-        expect(res.body.doc.name).toBe(products[0].name);
-      })
-      .end(done);
-  });
-
-  // Test case
-  it('should return a 404 if product is not found', (done) => {
-    var hexId = new ObjectID().toHexString();
-    request(app)
-      .get(`/products/${hexId}`)
-      .expect(404)
-      .end(done);
-  });
-
-  // Test case
-  it('should return 404 for invalid ObjectID', (done) => {
-    var invalidId = '123';
-    request(app)
-      .get(`/products/${invalidId}`)
-      .expect(404)
-      .end(done);
-  })
 });
 
