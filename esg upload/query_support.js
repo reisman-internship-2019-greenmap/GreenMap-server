f80b8a168c1f0e8932fe5ed13523010ea8ca6fce
Mac <39278228+Reithger@users.noreply.github.com>
2019-07-23 00:57:16 -0300

    Do you like comments? Cause I made comments. Can't sleep.

--- esg upload/query_support.js
+++ esg upload/query_support.js
@@ -2,10 +2,42 @@ const assert = require('assert');
 const request = require('request');
 const fs = require("fs")
 
+/*
+  This module provides several methods that support programs interacting with asynchronous behavior, such
+  as file reading and url queries. Primarily, these methods generate Promise objects that can be resolved
+  en masse with a Promise.all([]).then... command to help keep the flow of logic tidy when having to make
+  many asynchronous requests for many distinct entities (such as ~4000 companies).
+
+  Included currently are:
+    - get_url_company: Function to get .json of a url ending in a provided company name
+    - get_url_json: Function to get .json of any provided url
+    - get_file_data: Function to get data out of a file, broken up by a .split("\r\n") into an array
+    - mongo_collection_insert: Function to insert a provided document into a similarly provided MongoDB collection
+*/
+
 module.exports = {
-  get_url_json : function(front_url, company){
-    var company_url = company.replace(/ /g, "%20")
-    return new Promise(function(resolve, reject){
+
+  /*
+    This function takes a generic url and a specific company title to retrieve the .json from the
+    specified site; makes assumptions about its usage such that it is going to append the provided
+    company to the given url after substituting any ' ' in the company for '%20', and returns an
+    object with the company's name as a field and the resulting .json its body.
+
+    Parameters:
+      - front_url: url of a website to retrieve specified data from
+      - company: name of a company to specify the retrieval of via the front_url
+    Returns:
+      - A Promise object that Resolves with:
+        - Object of the following form:
+          - {company: [the provided company name],
+             body: [the retrieved .json object]}
+         OR:
+        - null if an error occurs
+  */
+
+  get_url_company : function(front_url, company){
+    var company_url = company.replace(/ /g, "%20")      //urls don't like spaces, use %20 instead
+    return new Promise(function(resolve, reject){       //Wrap a request in a promise so we can properly interact with it and await its completion
       request(front_url + company_url + ".json", function(err, response, body){
         try{
           resolve({company: company, body: JSON.parse(body)});           //JSON parse the result, if not in json format we try again forever (need better way, probably)
@@ -16,6 +48,60 @@ module.exports = {
       });
     });
   },
+
+  /*
+  This function takes any provided url, ensures that it ends in ".json", and then
+  makes a request to retrieve the .json object that the url leads to. This is a
+  very generic format that relies on the provided url to be accurate, unlike
+  get_url_company which does some processing and formatting of its parameters.
+
+  Parameters:
+    - url: String representing a url to query to retireve a .json object
+    - verbose: Boolean value denoting whether or not to print information to the console that is relevant;
+    in this case being error messages.
+
+  Returns:
+    - A Promise object that Resolves with:
+      - .json object retrieved from the url
+      OR:
+      - null in the case of an error
+
+  */
+
+  get_url_json: function(url, verbose){
+    return new Promise(function(resolve, reject){
+      if(!url.match(".*\.json")){
+        url = url + ".json"
+      }
+      request(url, function(err, response, body){
+        try{
+          resolve(JSON.parse(body));
+        }
+        catch(err){
+          if(verbose){
+            console.log(url);
+            console.log(err);
+          }
+          resolve(null);
+        }
+      });
+    });
+
+  },
+
+  /*
+    This function accesses a file denoted by the provided file_path and returns an array
+    containing that file's contents, each entry in the array being a line from the file
+    split using "\r\n".
+
+    Parameters:
+      - file_path: a string representing a file location on the computer running this function
+
+    Returns:
+      - A Promise object that Resolves with:
+        - data: an array containing the contents of the specified file, split using "\r\n".
+
+  */
 
   get_file_data : function(file_path){
     return new Promise(function(resolve, reject){   //Read a file, depending on which one it is we have a different format for reading.
@@ -28,6 +114,23 @@ module.exports = {
       });
     });
   },
+
+  /*
+    This function uses a provided MongoDB collection and document to insert the document into
+    that collection (basically putting a javascript object into MongoDB storage). If desired,
+    can print to the console each file that is inserted along with what was inserted, useful
+    for debugging.
+
+    Parameters:
+      - collection: A MongoDB Collection that we are inserting a document into
+      - doc: Some object that is being inserted into the MongoDB Collection provided
+      - verbose: A Boolean value specifying whether information is printed to the terminal or not.
+
+    Returns:
+      - A Promise that Resolves with Nothing.
+
+
+  */
 
   mongo_collection_insert : function(collection, doc, verbose){
     return new Promise(function(resolve, reject){
--- esg upload/test.js
+++ esg upload/test.js
@@ -1,28 +1,46 @@
 const query_support = require("./query_support.js");
 const MongoClient = require('mongodb').MongoClient;
 
+/*
+  This program populates a MongoDB database with information about companies by processing local files
+  to generate objects that additional information is appended to via requests to wikirates for that
+  company's aliases as well as its Greenscore.
+
+  In order:
+   - Read local Files
+     - Master List to generate all unique objects immediately
+     - 4 Files that have additional information which is appended to the previously generated company objects
+  - Query Wikirates for the Aliases associated to each company
+    - Will sometimes miss due to the name not being correct, can't do much about that. Many will hit, though.
+  - Query Wikirates for the Greenscore values associated to each company
+    - Again, will sometimes miss but previously obtained data from the 4 Files gives us some context.
+  - Connect to MongoDB Database, retrieve Collection, and insert all Company objects as Documents to it
+
+*/
+
 var files = ["Company-Master-List.txt", "Corporate-Knight-Clean.txt", "Newsweek-500-Clean.txt", "Newsweek-500-2-Clean.txt", "Yearbook-Robecosam-Clean.txt"];
+
 var alias_url = "https://wikirate.org/"
 var greenscore_url = "https://wikirate.org/Newsweek+Newsweek_Green_Score+"
 
-var companies = []
-var promises = []
+var companies = []      //Holds company objects as information is added on to them
+var promises = []       //Holds promises for Promise.all([]), gets re-used and reset for different promises along the way
 
 var i;
 
 for(i = 0; i < files.length; i++){    //Make file reading promises
-  promises.push(query_support.get_file_data(files[i]));
+  promises.push(query_support.get_file_data(files[i]));   //Calls function from query_support module to get promises
 }
 
 Promise.all(promises).then(function(values){  //Read files, make company objects from them
   console.log("Reading Files")
   for(i = 0; i < values.length; i++){
     console.log("Reading File: " + i)
-    var ls = values[i]
+    var ls = values[i]                        //Represents list of data from the file, each entry being one line
     var j;
     for(j = 0; j < ls.length; j++){
       var name = ls[j];
-      switch(i){
+      switch(i){                                  //Different conventions in the files place the name on separate line than default ls[j]
         case 2:
         case 3:
           name = ls[j+1];
@@ -30,11 +48,11 @@ Promise.all(promises).then(function(values){  //Read files, make company objects
         default:
           break;
       }
-      if(name == '' || name == undefined){
+      if(name == '' || name == undefined){        //Sometimes something weird happens, just ignore and move on
         continue;
       }
-      switch(i){
-        case 0:
+      switch(i){                          //Based on which file we're looking at, we do different things (usually what data to pull)
+        case 0:                           //Master List, make all of our base company objects
           var next = {company: name,
                     alias: [name],
                     category: [],
@@ -43,21 +61,21 @@ Promise.all(promises).then(function(values){  //Read files, make company objects
                     sustainable: "?"};
           companies[name] = next;
           break;
-        case 1:
+        case 1:                           //Corporate Knight, get sustainability score and a category type
           companies[name].category.push(ls[j+2]);
           companies[name].sustainable = ls[j+3];
-          j += 3;
+          j += 3;                         //This and the following file formats are grouped in sets of 4, so iterate
           break;
-        case 2:
+        case 2:                           //Newsweek, get a category type
           companies[name].category.push(ls[j+2]);
           j += 3;
           break;
         case 3:
-          name = ls[j+1]
+          name = ls[j+1]                  //Newsweek, get a category type
           companies[name].category.push(ls[j+2]);
           j += 3;
           break;
-        case 4:
+        case 4:                           //Yearbook Robecosam, get dow jones score and a category type
           companies[name].category.push(ls[j+1]);
           companies[name].dow = ls[j+3]
           j += 3;
@@ -68,7 +86,7 @@ Promise.all(promises).then(function(values){  //Read files, make company objects
   promises = []
   console.log("Generating Promises: Alias URL")
   for(i = 0; i < Object.keys(companies).length; i++){      //Make alias promises
-    promises.push(query_support.get_url_json(alias_url, Object.keys(companies)[i]));
+    promises.push(query_support.get_url_company(alias_url, Object.keys(companies)[i]));  //Module method to get generic .json from url query
   }
 
   Promise.all(promises).then(function(values){  //Query wikirates for company aliases
@@ -76,9 +94,9 @@ Promise.all(promises).then(function(values){  //Read files, make company objects
     for(i = 0; i < values.length; i++){
       if(values[i] != null){
         try{
-          if(values[i].body.hasOwnProperty("aliases")){
+          if(values[i].body.hasOwnProperty("aliases")){             //Avoids most errors, awkward and annoying to check each one but eh
             if(values[i].body.aliases.hasOwnProperty("content")){
-              companies[values[i].company].alias.push(values[i].body.aliases.content);
+              companies[values[i].company].alias.push(values[i].body.aliases.content);    //Append aliases to company object
             }
           }
         }
@@ -90,7 +108,7 @@ Promise.all(promises).then(function(values){  //Read files, make company objects
     promises = []
     console.log("Generating Promises: Greenscore URL")
     for(i = 0; i < Object.keys(companies).length; i++){    //Make greenscore promises
-      promises.push(query_support.get_url_json(greenscore_url, Object.keys(companies)[i]));
+      promises.push(query_support.get_url_company(greenscore_url, Object.keys(companies)[i])); //Module method, get generic .json from a different url
     }
 
     Promise.all(promises).then(function(values){    //Query wikirates for company greenscores
@@ -99,7 +117,7 @@ Promise.all(promises).then(function(values){  //Read files, make company objects
         if(values[i] != null){
           try{
             if(values[i].hasOwnProperty("items")){
-              companies[values[i].company].greenscore = values[i].items[0].value;
+              companies[values[i].company].greenscore = values[i].items[0].value;   //Set the greenscore value we found from the url
             }
           }
           catch(err){
@@ -108,25 +126,25 @@ Promise.all(promises).then(function(values){  //Read files, make company objects
         }
       }
 
-      const username = "patsy";
-      const password = "patsy";
+      const username = "patsy";                                 //Relevant information to access our MongoDB collection
+      const password = "patsy";                                 //Change this information for uploading to a different database
       const context = "greenmap";
-      const dbName = "sample_test";
+      const database_name = "sample_test";
       const url = "mongodb+srv://" + username + ":" + password + "@" + context + "-crohe.gcp.mongodb.net/test?retryWrites=true"
-      const collectionName = 'esg';
+      const collection_name = 'esg';
 
       MongoClient.connect(url, {useNewUrlParser: true}, function(err, client){    //Establish connection to the database
         console.log("MongoDB Client Open")
-        const db = client.db(dbName);
-        const collection = db.collection(collectionName);
+        const db = client.db(database_name);                                      //Get the Database
+        const collection = db.collection(collection_name);                        //Get the collection we want to insert to
 
         promises = []
         console.log("Generating Promises: MongoDB Insert")
-        for(i = 0; i < Object.keys(companies).length; i++){
+        for(i = 0; i < Object.keys(companies).length; i++){                       //Generate promises for inserting to our collection using the query_support module
           promises.push(query_support.mongo_collection_insert(collection, companies[Object.keys(companies)[i]], false));
         }
 
-        Promise.all(promises).then(function(values){
+        Promise.all(promises).then(function(values){            //After all promises resolved, close MongoDB connection and end
           console.log("Processing Results of Promises: MongoDB Insert")
           client.close();
           console.log("MongoDB Client Closed")
